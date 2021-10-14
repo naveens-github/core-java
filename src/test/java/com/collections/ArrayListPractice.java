6908f225111b08e038bf35dd2cdcdd04a103f579
Naveen <naveenwork8@gmail.com>
2021-10-13 23:30:36 -0400

    Added test case, create list example

--- src/test/java/com/collections/ArrayListPractice.java
+++ src/test/java/com/collections/ArrayListPractice.java
@@ -1,5 +1,15 @@
 package com.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
 public class ArrayListPractice {
+
+	@Test
+	public void create() {
+		List<String> list1 = new ArrayList<>();
+	}
 
 }
